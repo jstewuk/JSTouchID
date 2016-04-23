2072f7ff01db3129ef8adb01d05121630d98da49
Jim Stewart <jim@stewartstuff.com>
2016-04-22 23:01:39 -0500

    Organize view controller Create tab bar navigation Add picker for policies

--- JSTouchID/LocalAuthViewController.swift
+++ JSTouchID/LocalAuthViewController.swift
@@ -16,40 +16,62 @@ class LocalAuthViewController: UIViewController {
     let keychain = KeychainWrapper.standardKeychainAccess()
     
     // Local Authentication
-    var laContext = LAContext()
-    let policy = LAPolicy.DeviceOwnerAuthenticationWithBiometrics
+    var laContext: LAContext?
+    var policy: LAPolicy? = LAPolicy.DeviceOwnerAuthenticationWithBiometrics
+    let policies = [
+        "DeviceOwnerAuthentication",
+        "DeviceOwnerAuthenticationWithBiometrics"
+    ]
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBOutlet weak var policyPicker: UIPickerView!
+    
+    @IBOutlet weak var policyPickerTopConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        textView.editable = false
-        textView.layer.borderColor = UIColor.blueColor().CGColor
-        textView.layer.borderWidth = 2.0
+        configureTextView()
+        configureLAContext()
+        touchIDAvailable()
+    }
+}
 
-        touchIDAvailable()        
+//MARK: - LAContext -
+extension LocalAuthViewController {
+    private func configureLAContext() {
+        laContext = LAContext()
+        laContext?.localizedFallbackTitle = "Fallback Title"
     }
     
-    func touchIDAvailable() {
-        if laContext.canEvaluatePolicy(policy, error: nil) {
-            log( "Touch ID available")
-        } else {
-            print( "No Touch ID for you")
+    private func setLaPolicy(index: Int) {
+        switch index {
+        case 0:
+            policy = LAPolicy.DeviceOwnerAuthentication
+        case 1:
+            policy = LAPolicy.DeviceOwnerAuthenticationWithBiometrics
+        default:
+            policy = nil
+            log("Policy index: \(index) out of range")
         }
     }
     
-    func log(string: String) {
-        let text = textView.text ?? ""
-        textView.text = text + "\n" + string + "\n"
-        guard let viewText = textView.text else {return}
-        let range = NSMakeRange((viewText as NSString).length - 1, 1)
-        textView.scrollRangeToVisible(range)
+    func touchIDAvailable() {
+        let policy = LAPolicy.DeviceOwnerAuthenticationWithBiometrics
+        if ((laContext!.canEvaluatePolicy(policy, error: nil)) ) {
+            log( "Touch ID available")
+        } else {
+            log( "No Touch ID for you")
+        }
     }
+}
 
-    @IBOutlet weak var textView: UITextView!
+//MARK: - IBActions -
+extension LocalAuthViewController {
     
-    
-    @IBAction func localAuthTapped(sender: UIButton) {
-        log("local auth tapped")
-        laContext.evaluatePolicy(policy, localizedReason: "Local Authentication with TouchID") { (success: Bool, error: NSError?) in
+    @IBAction func authenticateTapped(sender: UIButton) {
+        log("Authenticate tapped")
+        laContext?.evaluatePolicy(policy!, localizedReason: "Local Authentication with TouchID") { (success: Bool, error: NSError?) in
             dispatch_async(dispatch_get_main_queue()) {
                 if success {
                     self.log("Successful local authentication with touch ID")
@@ -60,17 +82,57 @@ class LocalAuthViewController: UIViewController {
         }
     }
     
-    
-    @IBAction func resetLAContextTapped(sender: UIButton) {
-        log("Reset Local Auth Context Tapped")
-        laContext.invalidate()
-        laContext = LAContext()
-    }
-    
-    @IBAction func keychainAuthTapped(sender: UIButton) {
-        log("Keychain Auth tapped")
+    @IBAction func policyTapped(sender: UIButton) {
+        log("Policy tapped")
+        showPolicyPicker()
     }
 
+    @IBAction func resetLAContextTapped(sender: UIButton) {
+        log("Reset Local Auth Context Tapped")
+        laContext?.invalidate()
+        configureLAContext()
+    }
+    
+}
+
+//MARK:- Picker -
+extension LocalAuthViewController {
+    private func showPolicyPicker() {
+        policyPickerTopConstraint.constant = policyPicker.intrinsicContentSize().height
+        UIView.animateConstraint(view)
+    }
+    
+    private func hidePolicyPicker() {
+        policyPickerTopConstraint.constant = 0
+        UIView.animateConstraint(view)
+    }
+}
+
+
+extension LocalAuthViewController: UIPickerViewDelegate {
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        log("Policy: \(policies[row])")
+        setLaPolicy(row)
+        hidePolicyPicker()
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return policies[row]
+    }
+}
+
+extension LocalAuthViewController: UIPickerViewDataSource {
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return policies.count
+    }
+}
+
+//MARK: - Keychain stuff -
+extension LocalAuthViewController {
     
     func storeSecret() {
         let saveSuccessful: Bool = keychain.setString("Some String", forKey: key)
@@ -81,7 +143,37 @@ class LocalAuthViewController: UIViewController {
         let retrievedString = keychain.stringForKey(key)
         print("Retrieved string: \(retrievedString)")
     }
-    
-    
 }
+
+//MARK: - Logging TextView -
+extension LocalAuthViewController {
+    
+    private func configureTextView() {
+        textView.editable = false
+        textView.layer.borderColor = UIColor.blueColor().CGColor
+        textView.layer.borderWidth = 2.0
+    }
+    
+    
+    private func log(string: String) {
+        let text = textView.text ?? ""
+        textView.text = text + "\n" + string + "\n"
+        guard let viewText = textView.text else {return}
+        let range = NSMakeRange((viewText as NSString).length - 1, 1)
+        textView.scrollRangeToVisible(range)
+    }
+}
+
+private extension UIView {
+    class func animateConstraint(view: UIView) {
+        self.animateWithDuration(
+            0.25, delay: 0.0,
+            options: .CurveEaseInOut,
+            animations: {
+                view.layoutIfNeeded()
+            },
+            completion: nil)
+    }
+}
+
 
